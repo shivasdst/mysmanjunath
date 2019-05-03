cc8164c4bcfcd058ab44c20ecb60b45d512b3b34
Shivashankar <shivably04sdst@gmail.com>
2019-05-03 16:14:59 +0530

    About is in place

---
+++ application/views/flat/2-About/index.php
@@ -0,0 +1,77 @@
+<div class="container">
+  <div class="row gap-above-large">
+    <div class="col-md-12">
+      <h1 class="display-5 text-center gap-below-extralarge">About</h1>        
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+          <h3 class="divheading">The First Melody</h3>
+          <p class="h5 l-1-5">Amazingly gifted Violinist Dr. Mysore Manjunath, the son and disciple of renowned violinist Prof. S.Mahadevappa, was a remarkable child prodigy who matured quickly to emerge as one of the top ranking Violinist in India today. His exceptional musical talents were first revealed at very tender age of Eight when he stormed in to the music world sweeping all the music lovers & critics off their feet by his extraordinary musical acumen and technical virtuosity.</p>
+        </div>        
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo2.jpg" style="width: 100%;" class="img-responive" alt="" /> 
+        </div>
+    </div>   
+     <div class="row gap-above-large">
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo3.jpg" style="width: 100%;" class="img-responive" alt="" /> 
+        </div>
+        <div class="col-lg-6">
+          <h3 class="divheading">Journey</h3>
+          <p class="h5 l-1-5">From child prodigy to trail blazer, captivating audiences & critics, Dr.Manjunath has created an amazing record as Star performer in prestigious organizations world over.  From Royal Albert Hall in London to Sydney Opera House, from Common Thread Music festival in Oregon to Oxford University, from  Santa-Fe festival in New Mexico to World Music Festival in Chicago, from Esplanade Theatre in Singapore to Federation Squarein Melbourne, Dr.Manjunath has enthralled audiences all over the world.</p>
+        </div>        
+    </div>     
+    <div class="row gap-above-large">
+        <div class="col-lg-6">
+          <h3 class="divheading">Prince of Mysore</h3>
+          <p class="h5 l-1-5">The <strong>Los Angeles Times</strong> warmly noted that Manjunath has “crossed over so many boundaries, that his style is purely personal.”  He has brought laurels and glory to the field of Indian classical Music with the great sitar player Bharath Ratna Pandith Ravishankar admired and complemented his music and called him the <em>‘Prince of Mysore’</em> when he attended the live concert of Dr.Manjunath in Sandiego in 2012. Manjunath recently performed the first ever Indian Violin concert at the prestigious Oxford University in May 2018.</p>
+        </div>        
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo4.jpg" style="width: 100%;" class="img-responive" alt="" /> 
+        </div>
+    </div>    
+    <div class="row gap-above-large">
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo5.jpg" style="width: 100%;" class="img-responive" alt="" /> 
+        </div>
+        <div class="col-lg-6">
+          <h3 class="divheading">Academics</h3>
+          <p class="h5 l-1-5">Excelling in academics, Dr.Manjunath did his Bachelors and Master Degree in Music securing First Rank and Four Gold Medals from the University of Mysore. Subsequently, University of Mysore awarded him Ph.D for his scholarly thesis on Violin. University of Mysore accolades him as one among the alumni having the distinction and Stature of Eminence contributing to the society significantly. He is nominated the Cultural Ambassador by the University of Mysore during its centenary celebrations.</p>
+          <p class="h5 l-1-5">Dr. Manjunath, a distinguished Professor – Department of Music at The University of Mysore, has successfully guided many research students in the Ph.D program. Innumerable scholars and researchers from Europe and US, on Fulbright fellowship have studied under his Supervision. His methodology of analyzing different playing techniques & other intricate Music – Performing Arts have been followed by many reputed research scholars and musicologists.</p>
+          <p class="h5 l-1-5">Dr. Manjunath has the distinction of being invited for special Conferences, Presentations, Workshops & Concerts at some of the  World’s Top Universities including, The MIT- USA, Oxford University, University of Chicago, University of North Texas, IIT, The Kansas State University, University of Osaka Japan, University of Princeton, Griffith University – Australia, Texas A & M University – USA, University of Central Oklahoma, The University of Utrecht School of Arts – Netherlands.</p> 
+        </div>        
+    </div>
+    <div class="row gap-above-large">
+        <div class="col-lg-6">
+          <h3 class="divheading">Cultural Ambassador</h3>
+          <p class="h5 l-1-5">Violin Concerts of Dr. Manjunath have been featured in innumerable Music festivals & conferences all over the world. He is the first Indian violinist to be invited to perform at the Prestigious International Violin Conference held in Sandiego. He is the first ever Indian Musician to perform at the Persian Academy in The Islamic Republic Of Iran. He has performed at highly reputed  World Music festival in Chicago, Bozar Music conference in Belgium, Conservatorium Van Amsterdam and National Centre for Early Music – UK. The Government of India sent him to Slovenia for special Republic Day Concert at the Congress Hall as the Cultural Representative of our country. some of the Top European orchestras like Ictus, Okamoon have repeatedly featured him as a lead Violinist in various concert series in France, Germany, Netherlands. His concert at Reputed Milano Music Festival in Italy was telecasted live across Europe. He was invited to perform at reputed Putra World Trade Centre, Kuala Lumpur, Malaysia, SAARC summit, Theatre-dala-ville in Paris & Singapore Fine arts.</p> 
+          <p class="h5 l-1-5">World renowned BBCand ABC (Australian Broadcasting Corporation) featured him in their special international programs as the Brilliant Musician from India. Innumerable events he has been invited by Indian embassies abroad.  His dexterity in the subject made him one of the Front line Musicians of Indian classical Music. Recently he has been sent for a special Violin concert to Srilanka as a special  Cultural Delegate by Indian Government in connection with 70th Independence day celebrations.</p> 
+          <p class="h5 l-1-5">Dr. Manjunath has repeatedly enthralled audiences all over the world in over 30 countries. He is indeed true cultural ambassador of India who continues to preserve and spread the rich cultural heritage of Indian classical music, in all its glory.</p>
+        </div>        
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo6.jpg" style="width: 100%;" class="img-responive" alt="" /> 
+            <h3 class="divheading gap-above-medium">Awards & Accolades</h3>
+            <ul>
+              <li><span class="text-info">Sangeeth Natak Academy Award</span> – Dr. Manjunath is one of the youngest Recepient to receive this award from The President, Government of India.</li>
+              <li><span class="text-info">Rajyothsava Award</span> – Government of Karnataka</li>
+              <li><span class="text-info">The Excellent Award</span> – American Institute of World Culture</li>
+              <li><span class="text-info">Chowdiah National Award</span> – Academy of Music, Bangalore</li>
+              <li><span class="text-info">Best Violinist</span> – The Music Academy & Indian Fine Arts Society, Chennai</li>
+              <li><span class="text-info">Meritorious Award</span> – University of Oaklahoma, USA</li>
+              <li><span class="text-info">Honours from American Arts Council</span></li>
+              <li><span class="text-info">Pragya Award</span> – ACWS, New Delhi</li>
+              <li><span class="text-info">Rama Gana Kalacharya Award</span> – Shri Rama Seva Mandali, Bangalore</li>
+              <li><span class="text-info">Thantivadya Shiromani</span> – Sri Kanchi Kamakoti Peetham</li>
+              <li><span class="text-info">Aryabhata Award</span></li>
+              <li><span class="text-info">Sangeetha Samrat</span></li>
+              <li><span class="text-info">Sangitha Vidwanmani</span></li>
+              <li><span class="text-info">Ganavaridhi</span></li>
+              <li><span class="text-info">Sangitha Vedanta Dhurina</span></li>
+              <li><span class="text-info">Tatachari National Award</span></li>
+              <li><span class="text-info">Rotary Namana Excellence Award</span></li>
+              <li><span class="text-info">Samskruthi Sinchana</span> – Alva’s</li>
+            </ul>
+        </div>
+    </div>
+  </div>
+</div>
