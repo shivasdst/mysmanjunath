950c55ee592c9a9378463de6a6c9ada4fcefe0a9
Shivashankar <shivably04sdst@gmail.com>
2019-05-03 16:13:34 +0530

    Home page is in place

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -15,50 +15,43 @@
                 <div class="carousel-item active">
                   <img src="<?=STOCK_IMAGE_URL?>carousel1.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>Conduting symphony orchestra with 80 Artists</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel2.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>At prestigious Savai Gandharv Mahotsav</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel3.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>Concert at ISHA Foundation</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel4.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>Melbourne Concert with Guru Karaikudi Mani</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel6.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>At Saptak Ahmedabad</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel7.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>Virasat Mudabidre Concert</h4>
                   </div>
               </div>
               <div class="carousel-item">
                   <img src="<?=STOCK_IMAGE_URL?>carousel8.jpg" class="img-responsive" alt="...">
                   <div class="carousel-caption scheme-dark">
-                    <h3 class="caption-title">Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h4>With Umayalpuram Shivamani Sir & Shivamani</h4>
                   </div>
               </div>
           </div>
@@ -85,3 +78,17 @@
         </div>
     </div>
 </div>          
+
+<div class="container gap-above-large">
+    <div class="row">
+        <div class="col-lg-6">
+          <h3 class="divheading">The First Melody</h3>
+          <p class="h5 l-1-5">Amazingly gifted Violinist Dr. Mysore Manjunath, the son and disciple of renowned violinist Prof. S.Mahadevappa, was a remarkable child prodigy who matured quickly to emerge as one of the top ranking Violinist in India today. His exceptional musical talents were first revealed at very tender age of Eight when he stormed in to the music world sweeping all the music lovers & critics off their feet by his extraordinary musical acumen and technical virtuosity.</p>
+          <p class="h6 text-right"><a href="<?=BASE_URL?>About"class="text-info">Read more...</a></p>
+        </div>        
+        <div class="col-lg-6">
+           <img src="<?=STOCK_IMAGE_URL?>photo2.jpg" alt="" /> 
+        </div>
+    </div>
+</div>          
+
